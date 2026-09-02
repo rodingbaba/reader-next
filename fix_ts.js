0a01865042eccd6009756144a74ee4be841f4777
rodingbaba <rodingbaron@gmail.com>
2026-09-02 22:05:34 +0800

    chore: remove accidentally committed temporary script files

--- fix_ts.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-let code = fs.readFileSync('frontend/src/composables/useReaderAutoPlayback.ts', 'utf-8')
-
-code = code.replace(/  function getPrevParagraph\(\) \{\n    const current = getCurrentParagraph\(\)\n    return getPrevParagraphFrom\(current\)\n  \}\n\n/, '')
-
-fs.writeFileSync('frontend/src/composables/useReaderAutoPlayback.ts', code)
